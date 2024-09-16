5083416267740e0b0d5069b69b6d9e34a8935333
Tore Forsberg <erotfor@gmail.com>
2024-09-16 12:25:13 +0200

    Add ball that moves and bounces
    Co-Authored-By: Rickardssson <180270630+rickardssson@users.noreply.github.com>

---
+++ Ball.cs
@@ -0,0 +1,69 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace Breakout
+{
+    public class Ball
+    {
+        public Sprite Sprite;
+        public const float Diameter = 20.0f;
+        public const float Radius = Diameter * 0.5f;
+        public Vector2f Direction = new Vector2f(1, 1) / MathF.Sqrt(2.0f);
+        
+        public Ball()
+        {
+            this.Sprite = new Sprite();
+            this.Sprite.Texture = new Texture("assets/ball.png");
+            this.Sprite.Position = new Vector2f(250, 300);
+            Vector2f ballTextureSize = (Vector2f) Sprite.Texture.Size;
+            Sprite.Origin = 0.5f * ballTextureSize;
+            Sprite.Scale = new Vector2f(Diameter / ballTextureSize.X, Diameter / ballTextureSize.Y);
+        }
+
+        public void Update(float deltaTime)
+        {
+            var newPos = this.Sprite.Position;
+            newPos += this.Direction * deltaTime * 100.0f;
+            
+            // Collision right side of screen
+            if (newPos.X > Program.ScreenW - Radius)
+            {
+                newPos.X = Program.ScreenW - Radius;
+                Reflect(new Vector2f(-1, 0));
+            }
+            
+            // Collision left side of screen
+            if (newPos.X < Radius)
+            {
+                newPos.X = Radius;
+                Reflect(new Vector2f(1, 0));
+            }
+            
+            // Collision bottom of screen
+            if (newPos.Y > Program.ScreenH - Radius)
+            {
+                newPos.Y = Program.ScreenH - Radius;
+                Reflect(new Vector2f(0, -1));
+            }
+            
+            // Collision top of screen
+            if (newPos.Y < Radius)
+            {
+                newPos.Y = Radius;
+                Reflect(new Vector2f(0, 1));
+            }
+            
+            this.Sprite.Position = newPos;
+        }
+
+        public void Reflect(Vector2f normal)
+        {
+            this.Direction -= normal * (2 * (this.Direction.X * normal.X + this.Direction.Y * normal.Y));
+        }
+
+        public void Draw(RenderTarget target)
+        {
+            target.Draw(this.Sprite);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,25 +6,29 @@ namespace Breakout
 {
     class Program
     {
+        public const int ScreenW = 500;
+        public const int ScreenH = 700;
+
         static void Main(string[] args)
         {
             using (var window = new RenderWindow(
-                new VideoMode(500, 700), "Breakout"))
+                new VideoMode(ScreenW, ScreenH), "Breakout"))
             {
                 window.Closed += (o, e) => window.Close();
 
                 Clock clock = new Clock();
+                Ball ball = new Ball();
                 while (window.IsOpen)
                 {
                     float deltaTime = clock.Restart().AsSeconds();
                     window.DispatchEvents();
 
-                    // TODO: Updates
-
+                    // Updates
+                    ball.Update(deltaTime);
                     window.Clear(new Color(131, 197, 235));
 
-                    // TODO: Drawing
-
+                    // Drawing
+                    ball.Draw(window);
                     window.Display();
                 }
             }
