bda6439cee5fce523def2d695ae66a4d9426bdb9
Tore Forsberg <erotfor@gmail.com>
2024-09-16 11:38:30 +0200

    Initiate breakout app
    Co-Authored-By: Rickardssson <180270630+rickardssson@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Breakout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var window = new RenderWindow(
+                new VideoMode(500, 700), "Breakout"))
+            {
+                window.Closed += (o, e) => window.Close();
+
+                Clock clock = new Clock();
+                while (window.IsOpen)
+                {
+                    float deltaTime = clock.Restart().AsSeconds();
+                    window.DispatchEvents();
+
+                    // TODO: Updates
+
+                    window.Clear(new Color(131, 197, 235));
+
+                    // TODO: Drawing
+
+                    window.Display();
+                }
+            }
+        }
+    }
+}
